1a37aab7f425a454d1939cbfc71d6b21523cbec2
Thiago Senna <trsenna@gmail.com>
2023-05-07 22:35:35 +0000

    Prefer the use of a base local feature

--- src/node/.devcontainer/local-scripts/post-create-command.sh
+++ src/node/.devcontainer/local-scripts/post-create-command.sh
@@ -8,15 +8,19 @@ CURRENT_GROUP=$(id -g)
 #	Suspendisse sodales ipsum non justo imperdiet cursus.
 # -----------------------------------------------------------------------------------------------
 
-sudo mkdir -p /homestead/workspaces
-
 sudo chown $CURRENT_USER:$CURRENT_GROUP /homestead
 sudo chown $CURRENT_USER:$CURRENT_GROUP /homestead/code
+
+# Sed consectetur dictum arcu, sed interdum leo gravida sed
+sudo mkdir -p /homestead/workspaces
 sudo chown -R $CURRENT_USER:$CURRENT_GROUP /homestead/workspaces
 
 # -----------------------------------------------------------------------------------------------
 #	LOREM IPSUM
 #	Suspendisse sodales ipsum non justo imperdiet cursus.
 # -----------------------------------------------------------------------------------------------
+
+node --version \
+    && git --version
 
 echo "Done!"
--- src/php/.devcontainer/local-scripts/post-create-command.sh
+++ src/php/.devcontainer/local-scripts/post-create-command.sh
@@ -8,17 +8,23 @@ CURRENT_GROUP=$(id -g)
 #	Suspendisse sodales ipsum non justo imperdiet cursus.
 # -----------------------------------------------------------------------------------------------
 
-sudo mkdir -p /homestead/workspaces
-
 sudo chown $CURRENT_USER:$CURRENT_GROUP /homestead
 sudo chown $CURRENT_USER:$CURRENT_GROUP /homestead/code
+
+# Sed consectetur dictum arcu, sed interdum leo gravida sed
+sudo mkdir -p /homestead/workspaces
 sudo chown -R $CURRENT_USER:$CURRENT_GROUP /homestead/workspaces
 
+# Sed consectetur dictum arcu, sed interdum leo gravida sed
 sudo chown $CURRENT_USER:$CURRENT_GROUP /var/www/html
 
 # -----------------------------------------------------------------------------------------------
 #	LOREM IPSUM
 #	Suspendisse sodales ipsum non justo imperdiet cursus.
 # -----------------------------------------------------------------------------------------------
+
+php -version \
+    && git --version \
+    && node --version
 
 echo "Done!"
