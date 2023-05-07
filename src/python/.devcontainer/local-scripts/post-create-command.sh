427a20a27fe4b20127fff2c7916e96d7352ff966
Thiago Senna <trsenna@gmail.com>
2023-05-07 17:54:28 +0000

    Prefer homestead volume

--- src/python/.devcontainer/local-scripts/post-create-command.sh
+++ src/python/.devcontainer/local-scripts/post-create-command.sh
@@ -8,8 +8,11 @@ CURRENT_GROUP=$(id -g)
 #	Suspendisse sodales ipsum non justo imperdiet cursus.
 # -----------------------------------------------------------------------------------------------
 
-sudo chown $CURRENT_USER:$CURRENT_GROUP /code
-sudo chown $CURRENT_USER:$CURRENT_GROUP /workspaces
+sudo mkdir -p /homestead/workspaces
+
+sudo chown $CURRENT_USER:$CURRENT_GROUP /homestead
+sudo chown $CURRENT_USER:$CURRENT_GROUP /homestead/code
+sudo chown -R $CURRENT_USER:$CURRENT_GROUP /homestead/workspaces
 
 # -----------------------------------------------------------------------------------------------
 #	LOREM IPSUM
