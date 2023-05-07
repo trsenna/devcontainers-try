da7acd32b5e9c31b0b89a5aec088d697b45b7010
Thiago Senna <trsenna@gmail.com>
2023-05-07 18:00:42 +0000

    Add initial node devcontainer

---
+++ src/node/.devcontainer/local-scripts/post-create-command.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+CURRENT_USER=$(id -u)
+CURRENT_GROUP=$(id -g)
+
+# -----------------------------------------------------------------------------------------------
+#	LOREM IPSUM
+#	Suspendisse sodales ipsum non justo imperdiet cursus.
+# -----------------------------------------------------------------------------------------------
+
+sudo mkdir -p /homestead/workspaces
+
+sudo chown $CURRENT_USER:$CURRENT_GROUP /homestead
+sudo chown $CURRENT_USER:$CURRENT_GROUP /homestead/code
+sudo chown -R $CURRENT_USER:$CURRENT_GROUP /homestead/workspaces
+
+# -----------------------------------------------------------------------------------------------
+#	LOREM IPSUM
+#	Suspendisse sodales ipsum non justo imperdiet cursus.
+# -----------------------------------------------------------------------------------------------
+
+echo "Done!"
