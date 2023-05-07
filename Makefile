5daf78e355c388c3547f8f41e5bc866e67f238b3
Thiago Senna <trsenna@gmail.com>
2023-05-07 22:16:54 +0000

    Add initial working java devcontainer

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 build:
 	mkdir -p dist
+	(cd src && zip -r - ./java) > dist/devcontainers-try-java.zip
 	(cd src && zip -r - ./node) > dist/devcontainers-try-node.zip
 	(cd src && zip -r - ./php) > dist/devcontainers-try-php.zip
 	(cd src && zip -r - ./python) > dist/devcontainers-try-python.zip
---
+++ src/java/.devcontainer/local-scripts/post-create-command.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+CURRENT_USER=$(id -u)
+CURRENT_GROUP=$(id -g)
+
+# -----------------------------------------------------------------------------------------------
+#	LOREM IPSUM
+#	Suspendisse sodales ipsum non justo imperdiet cursus.
+# -----------------------------------------------------------------------------------------------
+
+sudo chown $CURRENT_USER:$CURRENT_GROUP /homestead
+sudo chown $CURRENT_USER:$CURRENT_GROUP /homestead/code
+
+# Sed consectetur dictum arcu, sed interdum leo gravida sed
+sudo mkdir -p /homestead/workspaces
+sudo chown -R $CURRENT_USER:$CURRENT_GROUP /homestead/workspaces
+
+# -----------------------------------------------------------------------------------------------
+#	LOREM IPSUM
+#	Suspendisse sodales ipsum non justo imperdiet cursus.
+# -----------------------------------------------------------------------------------------------
+
+java -version \
+    && git --version \
+    && node --version
+
+echo "Done!"
